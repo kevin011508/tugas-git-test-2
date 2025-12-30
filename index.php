668a569c078a3aa7679c98a241f60201b120eace
kevin011508 <abangkevin2008@gmail.com>
2025-12-30 14:56:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque pariatur omnis dolorum dolores voluptatem animi blanditiis ex sed nam doloremque facere sequi, ut ipsa nesciunt quaerat voluptatum in, praesentium delectus?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
